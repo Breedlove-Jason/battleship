a17f4eefbfb875c11d6868b8741819549c4e85b2
Jason Breedlove <j@jasonbreedlove.com>
2022-06-12 14:31:28 -0700

    view object added

--- battleship.js
+++ battleship.js
@@ -1,1 +1,23 @@
-let test = "test";
+let view = {
+  displayMessage: function (msg) {
+    let messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
+// test data
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Turn number: ");
