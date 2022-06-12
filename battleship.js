445ed444b64a9e614768eea09d59709c7938553e
Jason Breedlove <j@jasonbreedlove.com>
2022-06-12 15:59:37 -0700

    model complete
    troubleshooting test dataset

--- battleship.js
+++ battleship.js
@@ -12,15 +12,49 @@ let view = {
     cell.setAttribute("class", "miss");
   },
 };
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
 
-const model = {
+let model = {
   boardSize: 7,
   numShips: 3,
   shipLength: 3,
   shipsSunk: 0,
   ships: [
-    { location: [10, 20, 30], hits: ["", "", ""] },
-    { location: [32, 33, 34], hits: ["", "", ""] },
-    { location: [63, 64, 65], hits: ["", "", ""] },
+    { locations: [10, 20, 30], hits: ["", "", ""] },
+    { locations: [32, 33, 34], hits: ["", "", ""] },
+    { locations: [63, 64, 65], hits: ["", "", ""] },
   ],
+  fire: function (guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let locations = ship.locations;
+      let index = locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          this.shipsSunk++;
+          view.displayMessage("You sank my battleship!");
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  },
 };
